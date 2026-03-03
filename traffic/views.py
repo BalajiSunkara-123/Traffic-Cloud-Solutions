214a39ec40db3b23d30f87dc0b8ad443469ee8ea
BanothPradeepRaj <423115@student.nitandhra.ac.in>
2026-03-03 10:21:39 +0530

    BPK-36 HANDLE LOGIN SUCCESS/FAILURE

--- traffic/views.py
+++ traffic/views.py
@@ -4,15 +4,37 @@ from django.contrib import messages
 # from .models import Phonenumber
 # Create your views here.
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
 
 # def home(request):
 #     return HttpResponse("Traffic Cloud Solutions is running 🚦")
 
 def home(request):
     return render(request,'index.html')
+    
+# # if method==post
+#     take username password email
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        print(username)
 
-def login(request):
-    return render(request,'login.html')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("city/dashboard")   # make sure this URL name exists
+        else:
+            messages.error(request, "Invalid Username or Password")
+            return render(request, "login.html")
+    else :
+        return render(request, "login.html")
+
+# def login(request):
+#     return render(request,'login.html')
 
 def register(request):
     if(request.method=="POST"):
