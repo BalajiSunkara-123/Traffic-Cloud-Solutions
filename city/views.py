261f9587897645ffc0c8c063746e94dedffd9226
sunkara hari venkata balaji <sunkarabalaji288@gmail.com>
2026-08-29 20:02:47 +0530

    Deployment

--- city/views.py
+++ city/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponse
 # Create your views here.
 
 @login_required
@@ -8,5 +8,10 @@ def dashboard(request):
     return render(request,'dashboard.html')
     # return render(request,'traffic_dynamic.html')
 
+@login_required
 def trafficpolice(request):
-    return render(request,'traffic_police_dashboard.html')
+    if not request.session.get('is_traffic'):
+        return HttpResponse("Unauthorized Access", status=403)
+
+    return render(request, 'traffic_police_dashboard.html')
+
